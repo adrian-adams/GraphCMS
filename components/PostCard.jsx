b10d35795a7e78701f8f0dfaf6c874cf3292199b
helpimblind <adrian.adamsgit@outlook.com>
2025-08-11 00:15:42 +0200

    V1 - 110825
    New Project

---
+++ components/PostCard.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import moment from 'moment';
+import Link from 'next/link';
+
+const DateSVG = () => {
+  return (
+    <>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="#883407" d="M5.673 0a.7.7 0 0 1 .7.7v1.309h7.517v-1.3a.7.7 0 0 1 1.4 0v1.3H18a2 2 0 0 1 2 1.999v13.993A2 2 0 0 1 18 20H2a2 2 0 0 1-2-1.999V4.008a2 2 0 0 1 2-1.999h2.973V.699a.7.7 0 0 1 .7-.699M1.4 7.742v10.259a.6.6 0 0 0 .6.6h16a.6.6 0 0 0 .6-.6V7.756zm5.267 6.877v1.666H5v-1.666zm4.166 0v1.666H9.167v-1.666zm4.167 0v1.666h-1.667v-1.666zm-8.333-3.977v1.666H5v-1.666zm4.166 0v1.666H9.167v-1.666zm4.167 0v1.666h-1.667v-1.666zM4.973 3.408H2a.6.6 0 0 0-.6.6v2.335l17.2.014V4.008a.6.6 0 0 0-.6-.6h-2.71v.929a.7.7 0 0 1-1.4 0v-.929H6.373v.92a.7.7 0 0 1-1.4 0z"/></svg>
+    </>
+  )
+};
+
+const PostCard = ({post}) => {
+  return (
+    console.log(post),
+    <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
+        <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
+          <img 
+            src={post.featuredImage.url} 
+            alt={post.title} 
+            className='object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
+          />
+        </div>  
+        <h1 className='transition duration-100 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold inline'>
+          <Link href={`/post/${post.slug}`}>
+            {post.title}
+          </Link>
+        </h1>
+        <div className='block lg:flex text-center items-center justify-center mb-8 w-full'>
+          <div className='flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center'>
+            <img 
+              src={post.author.photo.url}
+              alt={post.author.name}
+              width="30px"
+              height="30px"
+              className='align-middle rounded-full'
+            />
+            <p className='inline align-middle text-gray-700 ml-2 text-lg'>
+              {post.author.name}
+            </p>
+          </div>
+          <div className='font-medium text-gray-700'>
+                <DateSVG className='h-6 w-6 inline mr-2' />
+                <span className='align-middle'>
+                  {moment(post.createdAt).format('MMM DD, YYYY')}
+                </span>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default PostCard
